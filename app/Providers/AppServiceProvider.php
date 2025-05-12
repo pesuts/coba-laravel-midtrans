ab81f0277bd743337d9d4e1c07ce6876c3b3fb8a
pesuts <daniel.sinaga.ds@gmail.com>
2025-05-12 20:07:27 +0700

    chore: create empty SQLite database during deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 use Midtrans\Config;
 
@@ -22,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+              // Buat file database SQLite jika belum ada
+      $path = database_path('database.sqlite');
+      if (!File::exists($path)) {
+          File::put($path, '');
+      }
+
       Config::$serverKey = config('midtrans.server_key');
       Config::$isProduction = config('midtrans.is_production');
       Config::$isSanitized = config('midtrans.is_sanitized');
